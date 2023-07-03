b5597106afccdfa708b0eaa7181c3d2fb8e19127
Wilson Piguing <wilsonpiguing@gmail.com>
2023-07-03 10:17:25 -0500

    API second pass
    Added view conditionals

--- NorthwesternDemo/Controllers/HomeController.cs
+++ NorthwesternDemo/Controllers/HomeController.cs
@@ -17,31 +17,36 @@ namespace NorthwesternDemo.Controllers
 
         public IActionResult Index()
         {
-
             return View();
         }
         public async Task<IActionResult> Api(string apiUrl) {
             
             if (string.IsNullOrEmpty(apiUrl)) {
+                // Sample Endpoints
+                // apiUrl = "https://api.publicapis.org/entries?category=sports";
                 apiUrl = "https://www.balldontlie.io/api/v1/teams";
             }
 
             using HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync(apiUrl);
 
+            ViewBag.StatusCode = response.StatusCode.ToString();
+            ViewBag.Url = apiUrl;
+
             if (response.IsSuccessStatusCode) {
                 string jsonString = await response.Content.ReadAsStringAsync();
-                //dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
-                //ViewBag.Result = jsonData;
-                ViewBag.Url = apiUrl;
-                ViewBag.Result = jsonString;
+                dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
+                ViewBag.Results = jsonData;
                 return View();
+            } else {
+                //Handle other responses if needed
             }
+            
             return View();
 
         }
 
-        public IActionResult MySql(string url)
+        public IActionResult MySql(string sqlUrl)
         {
 
             return View();
